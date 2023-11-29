81736a4cdf009fbc85fc809a31b9da6bccfd5e98
Nail Nadiroglu <nadiroglunail@gmail.com>
2023-11-28 22:17:51 -0500

    1 left

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+
+   const employee = {
+    name : "Nail",
+    streetAdress: "Bedford Ave"
+   } 
+   function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, 
+        [key]: value 
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+employee[key] = value;
+return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return {
+    ...employee,   
+  }
+}
+function nondestryctivelydeleteFromEmployeeByKey(employee, key) {
+  const modifiedEmployee = { ...employee };
+  delete modifiedEmployee[key];
+  return modifiedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
